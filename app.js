1ee395980e6e0cf554a86c854e61fa9f8dac9dcf
dianmjs <dianamjs69@gmail.com>
2020-06-09 10:57:21 -0500

    pares

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
+//operación
+const array = [];
+for (let i = 1; i <= 50; i++) {
+  array.push(`${i} Soy ${i % 2 == 0 ? "Par" : "Impar"}!`);
+}
+
 app.get("/", (req, res) => {
-  res.send("<p>Hola Mundo de nuevo</p>");
+  res.render("index", { numbers: array });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
